23ed0447a5d0da82856169f6dc434e2bb68cad8f
MH <gmhmhmhg.dev@gmail.com>
2023-05-22 21:45:42 +0900

    예외처리 / 수정 필요

--- src/test/java/com/example/account/controller/AccountControllerTest.java
+++ src/test/java/com/example/account/controller/AccountControllerTest.java
@@ -2,9 +2,11 @@ package com.example.account.controller;
 
 import com.example.account.domain.Account;
 import com.example.account.dto.*;
+import com.example.account.exception.AccountException;
 import com.example.account.type.AccountStatus;
 import com.example.account.service.AccountService;
 import com.example.account.service.RedisTestService;
+import com.example.account.type.ErrorCode;
 import com.fasterxml.jackson.databind.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -138,4 +140,24 @@ class AccountControllerTest {
                 .andExpect(jsonPath("$[2].balance").value(3000));
 
     }
+
+
+    @Test
+    void failGetAccount() throws Exception {
+        //given
+        given(accountService.getAccount(anyLong()))
+                .willThrow(new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
+        //when
+        //then
+        mockMvc.perform(get("/account/876"))
+                .andDo(print())
+                .andExpect(jsonPath("$.errorCode").value("ACCOUNT_NOT_FOUND"))
+                .andExpect(jsonPath("$.errorMessage").value("계좌가 없습니다"))
+                .andExpect(status().isOk());
+    }
+
+
+
+
+
 }
